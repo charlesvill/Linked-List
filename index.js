bdd70d1b0db9ab7ba406151293e5b37172acb736
Charles Villalpando <charlesvill@g.ucla.edu>
2023-11-08 21:54:22 -0800

    initial commit & first draft of linked list

---
+++ index.js
@@ -0,0 +1,132 @@
+
+
+// factory for linked list that will house all of the functions including creating a new node that will accept the value
+//
+
+const node = (value) => {
+  let next = null;
+  return {value, next};
+}
+
+const linkedList = () => {
+  let head = null;
+  let size = 0;
+  let listArr = [];
+  let listString = '';
+
+  const prepend = (input) => {
+    if (head === null){
+      head = node(input);
+      size += 1;
+      head.next = null;
+      console.log(head.value);
+    } else {
+      const root = head;
+      const newNode = node(input);
+      size += 1;
+      newNode.next = root;
+      head = newNode;
+      console.log(newNode.value)
+    }
+  }
+  const append = (input) => {
+    if (head === null){
+      head = node(input)
+      size += 1;
+      head.next = null;
+    } else {
+      const lastNode = returnLast();
+      const newNode = node(input);
+      size += 1;
+      lastNode.next = newNode;
+      newNode.next = null;
+      console.log(newNode.value);
+    }
+  }
+  const returnLast = (node = head) => {
+    if(node.next === null){
+      return node;
+    }
+    return returnLast(node.next);
+  }
+
+  const returnFirst = () => { return head;}
+
+  const returnIndexNode = (index) => {
+    if(index >= size){
+      console.error("Range Error: index exceeds list length");
+      return;
+    }
+
+    let currentNode = head;
+
+    for(let i = 0; i < size; i++){
+      if(i === index){
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    console.error("Something went wrong with looking for specific indexNode");
+  }
+
+  const popLast = () => {
+    lastNode = returnLast();
+    secondToLastNode = returnIndexNode(size - 2);
+    secondToLastNode.next = null;
+    lastNode = null;
+    size -= 1;
+  }
+  const unshiftFirst = () => {
+    const temp = head.next;
+    head = null;
+    head = temp;
+    size -= 1;
+  }
+
+  const length = () => { return size;}
+
+  const valueSearch = (value, node = head, i = 0) => {
+    if(node.value === value){
+      return true;
+    } else {
+      let currentNode = node.next;
+      if(i >= size || currentNode === null){
+        return false;
+      }
+      return valueSearch(value, currentNode, i + 1);
+    }
+  }
+
+
+
+  const stringify = () => {
+    let current = head;
+    for(let i = 0; i < size; i++){
+      if(current.next === null){
+        listString += `(${current.value})->(null)`;
+        console.log(listString);
+        return;
+      }      
+      listString += `(${current.value})->`;
+      current = current.next;
+    }
+  }
+  return {prepend, append, returnLast, returnFirst, length, returnIndexNode, unshiftFirst, popLast, valueSearch, stringify};
+};
+
+
+list = linkedList();
+list.prepend(1);
+list.prepend(3);
+list.prepend(7);
+list.prepend(5);
+list.append(120);
+list.popLast();
+list.unshiftFirst();
+console.log(list.valueSearch(7));
+list.stringify();
+console.log(list.length());
+
+
+// list.append(value)
+// const length = list.size()
